ed9c2e4198d2b8c0243b5b002b056fd99cf4d965
mathlet <mcdearmanchris@gmail.com>
2022-04-17 00:50:58 -0500

    add basic file reading

--- main.c
+++ main.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <string.h>
+#include <unistd.h>
+#include <limits.h>
 
 /*
  * Tokens
@@ -29,12 +31,18 @@ typedef struct {
     char* lit;
 } Token;
 
+char* token_pretty_str(Token* self) {
+    return self->lit;
+}
+
 /*
  * Lexer
  */
 
-Token* lex(char* str) {
+Token* lex(char* src) {
+    for (int i = 0; i < strlen(src); i++) {
 
+    }
 }
 
 /*
@@ -51,10 +59,41 @@ typedef enum {
  */
 
 Sexpr parse(Token* tokens) {
-
+    return Atom;
 }
 
+/*
+ * Eval
+ */
+
+/*
+ * Entry point
+ */
+
 int main() {
-    printf("Hello, World!\n");
+    char cwd[PATH_MAX];
+    if (getcwd(cwd, sizeof(cwd)) != NULL) {
+        printf("Current working dir: %s\n", cwd);
+    } else {
+        perror("getcwd() error");
+        return 1;
+   }
+
+    int c;
+    int i = 0;
+    char *src;
+    snprintf(cwd, sizeof(cwd), "%s", "../examples/simple.lic");
+    FILE* file = fopen(cwd, "r");
+    if (file) {
+        while ((c = getc(file)) != EOF) {
+            src[i++] = c;
+        }
+        fclose(file);
+    } else {
+        printf("File \"%s\" does not exist!", cwd);
+    }
+//    printf("Source: %s", src);
+    Token* tokens = lex(src);
+    printf("%v", tokens);
     return 0;
 }
