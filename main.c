dccd634a4c0843ca250d5607b37b50e5241b7ab4
mathlet <mcdearmanchris@gmail.com>
2022-04-18 15:36:43 -0500

    fix file reading and start lexing

--- main.c
+++ main.c
@@ -3,61 +3,54 @@
 #include <unistd.h>
 #include <limits.h>
 #include <stdlib.h>
+#include <stdbool.h>
+#include <ctype.h>
 
 //=================================================================
-// Helper Types
+// Helpers
 //=================================================================
+
+const unsigned long MAX_TOKENS = 10000;
 
 typedef struct {
     FILE *file;
 } File;
 
-char* ReadFile(char *filename)
+File *new_file(char* path) {
+    File *f = malloc(sizeof(File));
+    f->file = malloc(sizeof(FILE));
+    f->file = fopen(path, "r");
+    if (!f->file) {
+        printf("Error: file \"%s\" does not exist!", path);
+        exit(1);
+    }
+    return f;
+}
+
+char *file_to_str(File *self)
 {
     char *buffer = NULL;
-    int string_size, read_size;
-    FILE *handler = fopen(filename, "r");
+    long string_size;
+    unsigned long read_size;
+    FILE *handler = self->file;
 
     if (handler)
     {
-        // Seek the last byte of the file
         fseek(handler, 0, SEEK_END);
-        // Offset from the first to the last byte, or in other words, filesize
         string_size = ftell(handler);
-        // go back to the start of the file
         rewind(handler);
-
-        // Allocate a string that can hold it all
         buffer = (char*) malloc(sizeof(char) * (string_size + 1) );
-
-        // Read it all in one operation
         read_size = fread(buffer, sizeof(char), string_size, handler);
-
-        // fread doesn't set it so put a \0 in the last position
-        // and buffer is now officially a string
         buffer[string_size] = '\0';
-
         if (string_size != read_size)
         {
-            // Something went wrong, throw away the memory and set
-            // the buffer to NULL
             free(buffer);
             buffer = NULL;
         }
-
-        // Always remember to close the file.
         fclose(handler);
     }
 
     return buffer;
-}
-
-File *new_file(char* path) {
-    File *f;
-    f->file = fopen(path, "r");
-    if (!f->file)
-        printf("Error: file \"%s\" does not exist!", path);
-    return f;
 }
 
 int file_next(File *self) {
@@ -76,7 +69,7 @@ int file_peek(File *self) {
 //=================================================================
 
 typedef enum {
-    TOK_EOF = -1,
+    TOK_EOF,
     ILLEGAL,
 
     IDENT,
@@ -94,17 +87,100 @@ typedef enum {
     LET
 } TokenType;
 
+static const char* TOKEN_TYPE_STR[] = {
+        [TOK_EOF] = "<EOF>",
+        [ILLEGAL] = "<ILLEGAL>",
+        [IDENT] = "<IDENT>",
+        [INT] = "<INT>",
+        [ADD] = "+",
+        [SUB] = "-",
+        [MUL] = "*",
+        [QUO] = "/",
+        [MOD] = "%",
+        [LPAREN] = "(",
+        [RPAREN] = ")"
+};
+
 typedef struct {
     TokenType type;
-    char* lit;
+    char *lit;
 } Token;
+
+char *token_to_str(Token *self) {
+    const char *tok_type_str = TOKEN_TYPE_STR[self->type];
+    char *tok_str = malloc(sizeof("Token { Type: , Lit:  }")+sizeof(tok_type_str)+sizeof(self->lit));
+    snprintf(tok_str, sizeof(tok_str), "Token { Type: %s, Lit: %s }", tok_type_str, self->lit);
+    printf("%s\n", tok_type_str);
+    return tok_str;
+}
+
+Token new_token(TokenType type, char *lit) {
+    Token tok;
+    tok.type = type;
+    tok.lit = lit;
+    return tok;
+}
 
 //=================================================================
 // Lexer
 //=================================================================
 
-Token* lex(FILE *src) {
+bool is_whitespace(int c) {
+    return c == ' ' || c == '\t' || c == '\f' || c == '\v';
+}
 
+Token read_number(File *src) {
+    int c;
+    char *n = malloc(4*sizeof(char));
+    while ((isdigit(c = file_peek(src)))) {
+        n += c;
+        file_next(src);
+    }
+    return new_token(INT, n);
+}
+
+Token *lex(File *src) {
+    Token *tokens = malloc(MAX_TOKENS * sizeof(Token));
+    int c;
+    int i = 0;
+    while ((c = file_peek(src)) != EOF) {
+        printf("char: %c\n", c);
+        switch (c) {
+            case '+':
+                tokens[i++] = new_token(ADD, TOKEN_TYPE_STR[ADD]);
+                file_next(src);
+            case '-':
+                tokens[i++] = new_token(SUB, TOKEN_TYPE_STR[SUB]);
+                file_next(src);
+            case '*':
+                tokens[i++] = new_token(MUL, TOKEN_TYPE_STR[MUL]);
+                file_next(src);
+            case '/':
+                tokens[i++] = new_token(QUO, TOKEN_TYPE_STR[QUO]);
+                file_next(src);
+            case '%':
+                tokens[i++] = new_token(MOD, TOKEN_TYPE_STR[MOD]);
+                file_next(src);
+            case '(':
+                tokens[i++] = new_token(LPAREN, TOKEN_TYPE_STR[LPAREN]);
+                file_next(src);
+            case ')':
+                tokens[i++] = new_token(RPAREN, TOKEN_TYPE_STR[RPAREN]);
+                file_next(src);
+            default:
+                if (is_whitespace(c)) {
+                    file_next(src);
+                } else if (isdigit(c)) {
+                    tokens[i++] = read_number(src);
+                } else {
+                    char *ch = malloc(sizeof(char));
+                    snprintf(ch, sizeof(char), "%c", c);
+                    tokens[i] = new_token(ILLEGAL, ch);
+                    file_next(src);
+                }
+        }
+    }
+    return tokens;
 }
 
 //=================================================================
@@ -120,7 +196,7 @@ typedef enum {
 // Parser
 //=================================================================
 
-Sexpr parse(Token* tokens) {
+Sexpr parse(Token *tokens) {
     return Atom;
 }
 
@@ -128,29 +204,26 @@ Sexpr parse(Token* tokens) {
 // Eval
 //=================================================================
 
+int eval(Sexpr *sexpr) {
+    return 0;
+}
+
 //=================================================================
 // Entry Point
 //=================================================================
 
 int main() {
     char cwd[PATH_MAX];
-    if (getcwd(cwd, sizeof(cwd)) != NULL) {
-        printf("Current working dir: %s\n", cwd);
-    } else {
+    if (getcwd(cwd, sizeof(cwd)) == NULL) {
         perror("getcwd() error");
         return 1;
     }
     snprintf(cwd, sizeof(cwd), "%s", "../examples/simple.lic");
     File *file = new_file(cwd);
-
-    int c;
-    while ((c = file_peek(file) != EOF)) {
-        putchar(c);
-    }
-
-//    Token* tokens = lex(src);
-//    printf("%v", tokens);
-//    TokenType tk = ILLEGAL;
-//    printf("Illegal: %d\n", tk);
+    lex(file);
+//    Token* tokens = lex(file);
+//    for (int i = 0; i < sizeof(*tokens); i++) {
+//        printf("%s\n", token_to_str(&tokens[i]));
+//    }
     return 0;
 }
