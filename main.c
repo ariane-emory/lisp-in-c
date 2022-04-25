78be010ec0c67d4f6d4b8b89195e581fdb1ef2b1
mathlet <mcdearmanchris@gmail.com>
2022-04-25 08:53:28 -0500

    update list parse

--- main.c
+++ main.c
@@ -11,20 +11,33 @@
 //=================================================================
 
 const unsigned int MAX_TOKENS = 10000;
+const unsigned int MAX_OBJECTS = 10000;
 
 typedef struct {
     FILE *file;
 } File;
 
-File *new_file(char* path) {
+File *open_file(char* filename) {
+    char path[PATH_MAX];
+    if (!getcwd(path, sizeof(path))) {
+        perror("getcwd() error");
+        exit(1);
+    }
+    snprintf(path, PATH_MAX*sizeof(char), "../%s", filename);
+
     File *f = malloc(sizeof(File));
-    f->file = malloc(sizeof(FILE));
     f->file = fopen(path, "r");
     if (!f->file) {
         printf("Error: file \"%s\" does not exist!", path);
         exit(1);
     }
     return f;
+}
+
+void close_file(File *file) {
+    fclose(file->file);
+    free(file);
+    file = NULL;
 }
 
 char *file_to_str(File *self)
@@ -53,22 +66,22 @@ char *file_to_str(File *self)
     return buffer;
 }
 
-int file_next(File *self) {
-    return fgetc(self->file);
+char file_next(File *self) {
+    return (char) fgetc(self->file);
 }
 
 char file_peek(File *self) {
     int c;
     c = fgetc(self->file);
     ungetc(c, self->file);
-    return c;
+    return (char) c;
 }
 
 //=================================================================
 // Tokens
 //=================================================================
 
-typedef enum {
+typedef enum TokenType {
     TOK_EOF,
     ILLEGAL,
 
@@ -130,29 +143,44 @@ char *token_to_str(Token *self) {
     return tok_str;
 }
 
+//typedef struct {
+//    int pos;
+//    Token *tokens;
+//} TokenStream;
+//
+//Token *tok_next(TokenStream *self) {
+//    return &self->tokens[0];
+//}
+
 //=================================================================
 // Lexer
 //=================================================================
 
-bool is_whitespace(int c) {
+bool is_whitespace(char c) {
     return c == ' ' || c == '\t' || c == '\f' || c == '\v';
 }
 
 Token read_number(File *src) {
     char c;
     int idx = 0;
-    char *n = calloc(4, sizeof(char));
+    char n[31] = "";
     while ((isdigit(c = file_peek(src)))) {
+        if (strlen(n) >= 31) {
+            return new_token(ILLEGAL, (char *) TOKEN_TYPE_STR[ILLEGAL]);
+        }
         n[idx++] = c;
         file_next(src);
     }
-    return new_token(INT, n);
+    char *num = calloc(31, sizeof(char));
+    strcpy(num, n);
+    return new_token(INT, num);
 }
 
 Token read_ident(File *src) {
-    char c;
     int idx = 0;
     char ident[256] = "";
+//    char c = file_peek(src);
+    char c;
     while (isalnum(c = file_peek(src)) || c == '_') {
         if (strlen(ident) >= 256) {
             return new_token(ILLEGAL, (char *) TOKEN_TYPE_STR[ILLEGAL]);
@@ -204,18 +232,15 @@ Token *lex(File *src) {
             default:
                 if (is_whitespace(c)) {
                     file_next(src);
-                    break;
                 } else if (isdigit(c)) {
                     tokens[idx++] = read_number(src);
-                    break;
                 } else if (isalpha(c) || c == '_') {
                     tokens[idx++] = read_ident(src);
-                    break;
                 } else {
                     tokens[idx] = new_token(ILLEGAL, &c);
                     file_next(src);
-                    break;
                 }
+                break;
         }
     }
     return tokens;
@@ -225,32 +250,145 @@ Token *lex(File *src) {
 // Ast
 //=================================================================
 
-typedef enum {
-    Atom,
-    List
-} Sexpr;
+
+typedef struct Object Object;
+
+typedef enum Type {
+    NUMBER,
+    SYMBOL,
+    CONS,
+    ENV
+} Type;
+
+struct Object {
+    Type type;
+    size_t size;
+    union {
+        struct { long number; };                        // number
+        struct { Object *car, *cdr; };                  // cons
+        struct { char *name; };                         // symbol
+//        struct { Object *parent, *vars, *vals; };       // env
+//        struct { Object *forward; };                    // forwarding pointer
+    };
+};
+
+Object *new_object(Type type) {
+    Object *obj = calloc(1, sizeof(Object));
+    obj->type = type;
+    return obj;
+}
+
+Object *new_number(long n) {
+    Object *obj = new_object(NUMBER);
+    obj->number = n;
+    return obj;
+}
+
+Object *new_symbol(char *name) {
+    Object *obj = new_object(SYMBOL);
+    obj->name = name;
+    return obj;
+}
+
+char *ast_to_str(Object *ast) {
+
+}
 
 //=================================================================
 // Parser
 //=================================================================
 
-Sexpr parse(Token *tokens) {
-    int idx = 0;
+//Object *parse_atom(Token *tokens, int pos) {
+//    int idx = pos;
+//    while (tokens[idx].type != TOK_EOF) {
+//        Token tok = tokens[idx];
+//        switch (tok.type) {
+//            case IDENT:
+//                return new_symbol(tok.lit);
+//            case INT:
+//                return new_number(atol(tok.lit));
+//            default:
+//                break;
+//        }
+//    }
+//}
+
+Object *parse_list(Token *tokens, int pos) {
+    int idx = pos;
+    int obj_idx = 0;
+    Object objs[MAX_OBJECTS];
     while (tokens[idx].type != TOK_EOF) {
-        printf("%s\n", token_to_str(&tokens[idx++]));
+        Token tok = tokens[idx];
+        switch (tok.type) {
+            case IDENT:
+                objs[obj_idx++] = *new_symbol(tok.lit);
+                break;
+            case INT:
+                objs[obj_idx++] = *new_number(atol(tok.lit));
+                break;
+            case LPAREN:
+                objs[obj_idx++] = *parse_list(tokens, idx);
+                break;
+            case RPAREN:
+                break;
+            default:
+                break;
+        }
     }
-    return Atom;
+    Object *list = calloc(obj_idx+1, sizeof(Object));
+    memcpy(list, objs, obj_idx+1*sizeof(Object));
+
+    return list;
 }
 
-Sexpr parse_atom() {
-    return List;
+Object *parse(Token *tokens) {
+    int idx = 0;
+    int obj_idx = 0;
+    Object objs[MAX_OBJECTS];
+    while (tokens[idx].type != TOK_EOF) {
+        printf("%s\n", token_to_str(&tokens[idx++]));
+        Token tok = tokens[idx];
+        switch (tok.type) {
+            case IDENT:
+                objs[obj_idx++] = *new_symbol(tok.lit);
+                break;
+            case INT:
+                objs[obj_idx++] = *new_number(atol(tok.lit));
+                break;
+            case LPAREN:
+                objs[obj_idx++] = *parse_list(tokens, idx);
+                break;
+            default:
+                printf("error: invalid top level token: %s", token_to_str(&tok));
+                exit(1);
+        }
+        idx++;
+    }
+    Object *ast = calloc(obj_idx+1, sizeof(Object));
+    memcpy(ast, objs, obj_idx+1*sizeof(Object));
+
+    return ast;
 }
+
+//=================================================================
+// Garbage Collection
+//=================================================================
+
+void init() {
+
+}
+
+void allocate(int n) {
+
+}
+
+void collect(int n) {}
 
 //=================================================================
 // Eval
 //=================================================================
 
-int eval(Sexpr *sexpr) {
+int eval(Object objects[]) {
     return 0;
 }
 
@@ -259,17 +397,15 @@ int eval(Sexpr *sexpr) {
 //=================================================================
 
 int main() {
-    char cwd[PATH_MAX];
-    if (!getcwd(cwd, sizeof(cwd))) {
-        perror("getcwd() error");
-        return 1;
-    }
-    snprintf(cwd, PATH_MAX*sizeof(char), "%s", "../examples/simple.lic");
-    File *file = new_file(cwd);
+    File *file = open_file("examples/simple.lic");
     Token* tokens = lex(file);
-    int idx = 0;
-    while (tokens[idx].type != TOK_EOF) {
-        printf("%s\n", token_to_str(&tokens[idx++]));
-    }
+    close_file(file);
+//    int idx = 0;
+//    while (tokens[idx].type != TOK_EOF) {
+//        printf("%s\n", token_to_str(&tokens[idx++]));
+//    }
+    Object *ast = parse(tokens);
+    printf(ast_to_str(ast));
+
     return 0;
 }
