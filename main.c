d8a5c82245d5fc698ef9443037fdd007c8bdf8f6
mathlet <mcdearmanchris@gmail.com>
2022-04-25 19:42:11 -0500

    added parsing nonsense

--- main.c
+++ main.c
@@ -200,6 +200,7 @@ Token *lex(File *src) {
     char c;
     int idx = 0;
     while ((c = file_peek(src)) != EOF) {
+        printf("char: %c\n", c);
         switch (c) {
             case '+':
                 tokens[idx++] = new_token(ADD, (char *) TOKEN_TYPE_STR[ADD]);
@@ -315,28 +316,36 @@ char *ast_to_str(Object *ast) {
 
 Object *parse_list(Token *tokens, int pos) {
     int idx = pos;
-    int obj_idx = 0;
-    Object objs[MAX_OBJECTS];
-    while (tokens[idx].type != TOK_EOF) {
-        Token tok = tokens[idx];
+    Object cons;
+    while (tokens[idx++].type != TOK_EOF) {
+        Token tok = tokens[idx-1];
         switch (tok.type) {
             case IDENT:
-                objs[obj_idx++] = *new_symbol(tok.lit);
+            case ADD:
+            case SUB:
+            case MUL:
+            case QUO:
+            case MOD:
+                cons.car = new_symbol(tok.lit);
+                cons.cdr = parse_list(tokens, idx-1);
                 break;
             case INT:
-                objs[obj_idx++] = *new_number(atol(tok.lit));
+                cons.car = new_number(strtol(tok.lit, NULL, 10));
+                cons.cdr = parse_list(tokens, idx-1);
                 break;
             case LPAREN:
-                objs[obj_idx++] = *parse_list(tokens, idx);
+                cons.car = parse_list(tokens, idx-1);
+                cons.cdr = parse_list(tokens, idx);
                 break;
             case RPAREN:
                 break;
             default:
-                break;
+                printf("error: invalid list token: %s", token_to_str(&tok));
+                exit(1);
         }
     }
-    Object *list = calloc(obj_idx+1, sizeof(Object));
-    memcpy(list, objs, obj_idx+1*sizeof(Object));
+    Object *list = calloc(idx-pos+1, sizeof(Object));
+    memcpy(list, &cons, (idx-pos+1)*sizeof(Object));
 
     return list;
 }
@@ -345,24 +354,28 @@ Object *parse(Token *tokens) {
     int idx = 0;
     int obj_idx = 0;
     Object objs[MAX_OBJECTS];
-    while (tokens[idx].type != TOK_EOF) {
+    while (tokens[idx++].type != TOK_EOF) {
         printf("%s\n", token_to_str(&tokens[idx++]));
-        Token tok = tokens[idx];
+        Token tok = tokens[idx-1];
         switch (tok.type) {
             case IDENT:
+            case ADD:
+            case SUB:
+            case MUL:
+            case QUO:
+            case MOD:
                 objs[obj_idx++] = *new_symbol(tok.lit);
                 break;
             case INT:
-                objs[obj_idx++] = *new_number(atol(tok.lit));
+                objs[obj_idx++] = *new_number(strtol(tok.lit, NULL, 10));
                 break;
             case LPAREN:
-                objs[obj_idx++] = *parse_list(tokens, idx);
+                objs[obj_idx++] = *parse_list(tokens, idx-1);
                 break;
             default:
                 printf("error: invalid top level token: %s", token_to_str(&tok));
                 exit(1);
         }
-        idx++;
     }
     Object *ast = calloc(obj_idx+1, sizeof(Object));
     memcpy(ast, objs, obj_idx+1*sizeof(Object));
@@ -397,7 +410,7 @@ int eval(Object objects[]) {
 //=================================================================
 
 int main() {
-    File *file = open_file("examples/simple.lic");
+    File *file = open_file("examples/test.lic");
     Token* tokens = lex(file);
     close_file(file);
 //    int idx = 0;
