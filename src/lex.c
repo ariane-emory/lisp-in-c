dfe9b148248c5b769661414fc1f47dcbcba52552
ariane-emory <ariane.emory@protonmail.com>
2022-05-08 19:32:34 -0400

    ...

--- src/lex.c
+++ src/lex.c
@@ -29,7 +29,7 @@ Token read_number(File *src) {
     n[idx] = 0;
     
     char *num = calloc(32, sizeof(char));
-    LOGCALLOC(num, 32, char);
+    LOGCALLOC(num, char, 32);
     
     strcpy(num, n);
     Token tok = new_token(TOK_INT, num);
