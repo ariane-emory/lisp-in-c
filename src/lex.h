dc2de510dc186eaf5009a503fa4b3883d53fae80
mathlet <mcdearmanchris@gmail.com>
2022-06-09 22:17:12 -0500

    add TokenStream implementation

--- src/lex.h
+++ src/lex.h
@@ -1,8 +1,8 @@
 #ifndef LEXER_H
 #define LEXER_H
 
-#include <string>
 #include "token.h"
+#include <string>
 #include <fstream>
 
 token::TokenStream lex(std::ifstream src);
---
+++ src/token.cpp
@@ -0,0 +1,11 @@
+#include "token.h"
+
+using namespace token;
+
+const Token &TokenStream::peek() const {
+  return this->tokens.at(this->pos);
+}
+
+const Token &TokenStream::next() {
+  return this->tokens.at(this->pos++);
+}
